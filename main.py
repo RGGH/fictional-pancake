61b8a4ebe8944943b7e9558a9062ab45be7466f8
RGGH <pisscompany@gmail.com>
2023-03-26 20:32:52 +0100

    alter @route

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ class request_body(BaseModel):
     earn: int
 
 
-@app.post("/pancake")
+@app.post("/")
 def vars(data: request_body)->dict:
     ## result
     rating = neural_net.predict([[data.weeks,data.years,data.books,data.projects,data.earn]])
